06dc10bdddd4336649d2f3e64a2e2691cdf07763
sofiamejiamuro <sofiamejiamuro@gmail.com>
2024-06-14 10:37:35 -0600

    Setup Node, express app with TS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,22 @@
-console.log(
-  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
-);
+import express from 'express';
+
+const app = express();
+/**EXAMPLES
+ * GET - PUT - POST - DELETE
+
+app.get("/hello", (req,res,next) =>{
+  console.log(req.body)
+  return res.send("Hello");
+});
+//dynamic route
+app.delete("/user/:id", (req,res,next) =>{
+  console.log(req.params.id)
+  return res.send("Hello");
+});
+ */
+
+//middlewares
+app.use(express.json());
+
+//connections and listeneres
+app.listen(3000, () => console.log("Server Open"))
